1a20ca8aa06343bc4c780a0234419a7b9c31d0e1
miekmiek <mhxi91@gmail.com>
2015-11-18 14:45:40 -0800

    fixed routes and links

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,34 +2,42 @@ class RidesController < ApplicationController
 
 	def new
 		@ride = Ride.new
+    render :new
 	end
 
 	def create
-		@ride = Rides.create(ride_params)
+		@ride = Ride.create(ride_params)
 		if @ride.save
 			redirect_to @ride
+    else
+      render :new
 		end
 	end
 
 	def edit
+    @ride = Ride.find(params[:id])
+    render :edit
 	end
 
 	def update
+    @ride = Ride.find(params[:id])
+    @ride.update_attributes(ride_params)
+    redirect_to @ride
 	end
 
 	def show
+    @ride = Ride.find(params[:id])
+    render :show
 	end
 
 	def destroy
+    Ride.destroy(params[:id])
+    redirect_to @user
 	end
 
 	private
-
-	def ride_params
-		params.require(:ride).permit(:title, :start_date, :start_time, :start_location)
-	end
-
-
-
+  	def ride_params
+  		params.require(:ride).permit(:title, :start_date, :end_date, :start_time, :end_time, :start_location, :end_location, :description, :distance, :drop, :public)
+  	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,47 @@
 class UsersController < ApplicationController
 
-	def new
-		@user = User.new
-	end
-
-	def create
-		@user = User.new(user_params)
-
-		if @user.save
-			session[:user_id] = @user.id
-			redirect_to new_user_path
-		else
-			render :new
-		end
-	end
-
-	def edit
-		@user = User.find(params[:id])
-	end
-
-	def update
-		@user = User.find(params[:id])
-		if @user.update_attributes(user_params_edit)
-		 redirect_to user_path(@user)
-		else
-		  render 'edit' 
-		end
-	end
-
 	def show
 		@user = User.find(params[:id])
+    render :show
 	end
 
-	private
+  def new
+    @user = User.new
+    render :new
+  end
 
-	def user_params
-		params.require(:user).permit(:email, :password, :password_confirmation, :name)
-	end
+  def create
+    @user = User.new(user_params)
 
-	def user_params_edit
-		params.require(:user).permit(:email, :name)
-	end
+    if @user.save
+      login(@user)
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params_edit)
+     redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  	end
+
+  	def user_params_edit
+  		params.require(:user).permit(:email, :name)
+  	end
 
 end
