ab1592717bfdb785e45f8867bb42a17a15b78723
miekmiek <mhxi91@gmail.com>
2015-11-23 15:50:56 -0800

    separate past/upcoming rides, hide ride attribute fields that are blank

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,4 @@
 class RidesController < ApplicationController
-	before_action :correct_user, only: :destroy
 	
 	def index
 	    @rides = Ride.near(params[:q], 10).reverse_order.all.paginate(page: params[:page], per_page: 5)
@@ -42,7 +41,8 @@ class RidesController < ApplicationController
 	end
 
 	def destroy
-		Ride.destroy(params[:id])
+    @ride = Ride.friendly.find(params[:id])
+		@ride.destroy
 		redirect_to user_path(current_user)
 	end
 
@@ -50,11 +50,6 @@ class RidesController < ApplicationController
 
 	def ride_params
 		params.require(:ride).permit(:title, :start_date, :end_date, :start_time, :end_time, :start_location, :end_location, :description, :drop, :public, :user_id)
-	end
-
-	def correct_user
-		@ride = current_user.ridess.find_by(id: params[:id])
-		redirect_to root_url if @ride.nil?
 	end
 
 end
